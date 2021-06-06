68cf4515d4dd4415b774309e2acf56f2392c50f7
arcursino <arcursino@gmail.com>
2021-06-06 19:50:30 +0000

    inclui axios

--- src/main.js
+++ src/main.js
@@ -2,6 +2,30 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './assets/style.css'
+import axios from 'axios'
+
+axios.interceptors.request.use(config => {
+  if(store.state.token) {
+    config.headers.Authorization = store.state.token
+  }
+  return config
+});
+
+axios.interceptors.response.use(res => {
+    return res
+  }, error => {
+    if(error.response.status === 403) {
+      alert('Não autorizado!')
+    } else if (error.response.status === 401) {
+       alert('Não autorizado!')
+      //store.commit('logout')
+      //router.push('')
+    }
+    throw error
+});
+
+axios.defaults.baseURL = "https://8080-olive-minnow-zuqdq6ay.ws-us08.gitpod.io/ecommerce/";
 
 Vue.config.productionTip = false
 
