e3d2490ce141a674b9ea46821ecaf9684acd89d6
arcursino <arcursino@gmail.com>
2021-06-06 19:32:40 +0000

    inclui rotas na aplicação

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,34 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" v-if="this.$store.state.token != null">
+      <router-link to="/home">Home</router-link> |
+      <router-link to="/clientes" v-if="this.$store.state.tipo == 'ROLE_ADMIN'">Clientes | </router-link> 
+      <router-link to="/pedidos" v-if="this.$store.state.tipo == 'ROLE_ADMIN'">Pedidos | </router-link>
+      <router-link to="/cadastro-cliente">Cadastro de Clientes</router-link> |
+      <router-link to="/cadastro-pedido">Cadastro de Pedido</router-link> |
+      <a><p @click="sair">Sair</p></a>
     </div>
+    
     <router-view/>
   </div>
 </template>
+
+<script>
+  export default {
+    name: "app",
+    data(){
+      return {
+        
+      }
+    },
+    methods: {
+      sair(){
+        this.$store.commit("logout");
+        this.$router.push("/");
+      }
+    }
+  }
+</script>
 
 <style>
 #app {
@@ -16,17 +38,11 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
