9071015d008b0405a4422dbf4cb8d087afd7c313
arcursino <arcursino@gmail.com>
2021-06-06 19:25:11 +0000

    inclui os componentes pedido e cliente

---
+++ src/components/LinhaCliente.vue
@@ -0,0 +1,24 @@
+<template>
+    <tr>
+        <td>
+            {{nome}}
+        </td>
+        <td>
+            {{email}}
+        </td>
+        <td>
+            {{idade}}
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: 'LinhaCliente',
+        props: {
+            nome: String,
+            email: String,
+            idade: Number
+        }
+    }
+</script>
---
+++ src/components/LinhaPedido.vue
@@ -0,0 +1,28 @@
+<template>
+    <tr>
+        <td>
+            {{id}}
+        </td>
+        <td>
+            {{nome}}
+        </td>
+        <td>
+            {{valor}}
+        </td>
+        <td>
+            {{cliente}}
+        </td>
+    </tr>
+</template>
+
+<script>
+    export default {
+        name: 'LinhaPedido',
+        props: {
+            id: Number,
+            nome: String,
+            valor: Number,
+            cliente: String
+        }
+    }
+</script>
